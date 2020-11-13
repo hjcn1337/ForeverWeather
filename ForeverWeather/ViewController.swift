504af20e15eb28ef382e8378070811ac1659baa1
Ростислав Ермаченков <errosgen@yandex.ru>
2020-11-13 10:53:36 +0300

    Getting data from api (test)

--- ForeverWeather/ViewController.swift
+++ ForeverWeather/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=78f0d5436db9172dc2780f7e006b2bf0"
+        let url = URL(string: urlString)!
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, response, error in
+            if let data = data {
+                let dataString = String(data: data, encoding: .utf8)
+                print(dataString!)
+                
+            }
+        }
+        
+        task.resume()
     }
 
 
