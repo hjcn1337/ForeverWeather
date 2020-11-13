3e25e48341f2146af6390aa6870322f766edd8d3
Ростислав Ермаченков <rostislavermacenkov@MacBook-Pro-Rostislav.local>
2020-11-13 10:08:53 +0300

    Initial commit

---
+++ ForeverWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForeverWeather
+//
+//  Created by Ростислав Ермаченков on 13.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
